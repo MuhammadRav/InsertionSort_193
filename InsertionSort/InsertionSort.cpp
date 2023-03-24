7ab32cda80f0174b6602f95280851cab83557cac
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-03-24 11:03:51 +0700

    e

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -64,4 +64,9 @@ void display() {
     cout << "\n=================================" << endl;
     cout << "Element Array yang telah tersusun" << endl;
     cout << "==================================" << endl;
+
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl
+    }
+    cout << endl;
 }
