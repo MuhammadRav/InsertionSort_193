e0929fb23c12460fd009d1be5b0f18962aca436f
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-03-24 11:05:25 +0700

    menambahkan ";" pada endl

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -67,7 +67,7 @@ void display() {
     cout << "==================================" << endl;
 
     for (int j = 0; j < n; j++) {
-        cout << arr[j] << endl
+        cout << arr[j] << endl;
     }
     cout << endl;
 }
