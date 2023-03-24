4b6849efb5dded2e837f88f4763fa72ece4fe0fd
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-03-24 10:18:16 +0700

    nambahin keterangan

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
+int arr[20];                                       // Membuat Array dengan panjang data 20
+int n;                                             // Membuat Variable inputan n
 
-void input() {
+void input() {                             // procedure input
     while (true)
     {
-        cout << "Masukkan jumlah data pada array : ";
-        cin >> n;
+        cout << "Masukkan jumlah data pada array : "; // Membuat inputan jumlah element Array
+        cin >> n;                                      // Memanggil Variable inputan n
         
-        if (n <= 20) {
+        if (n <= 20) {                                 // Membuat Kondisi n tidak lebih dari 20
             break;
         }
         else
         {
-            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
         }
     }
     cout << endl;
-    cout << "======================" << endl;
-    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;       // Membuat jarak per baris program
+    cout << "Masukkan Element Array" << endl;       // Membuat tampilan susunan data element array
     cout << "======================" << endl;
 }
