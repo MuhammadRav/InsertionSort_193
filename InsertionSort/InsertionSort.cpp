265765f3979a9533cc0d0ea8eaf3a07ea869438c
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-03-24 10:14:21 +0700

    p

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int arr[20];
+int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukkan jumlah data pada array : ";
+        cin >> n;
+        
+        if (n <= 20) {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
 }
-
